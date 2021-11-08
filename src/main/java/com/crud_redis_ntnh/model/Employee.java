a461444bac710ac3d3eaca4475319770fc233980
HaoSmall <nhathao00852@gmail.com>
2021-11-08 11:27:24 +0700

    crud list

--- src/main/java/com/crud_redis_ntnh/model/Employee.java
+++ src/main/java/com/crud_redis_ntnh/model/Employee.java
@@ -2,6 +2,7 @@ package com.crud_redis_ntnh.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
@@ -9,6 +10,7 @@ import java.io.Serializable;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class Employee implements Serializable {
     private int id;
     private String name;
--- src/main/java/com/crud_redis_ntnh/repository/EmployeeRepository.java
+++ src/main/java/com/crud_redis_ntnh/repository/EmployeeRepository.java
@@ -2,6 +2,7 @@ package com.crud_redis_ntnh.repository;
 
 import com.crud_redis_ntnh.model.Employee;
 import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -10,30 +11,51 @@ import java.util.List;
 @Repository
 public class EmployeeRepository {
 
+    private String HASH_KEY = "EMPLOYEE";
+    private String LIST_KEY = "EMPLOYEE-LIST";
+
     private HashOperations hashOperations;//crud hash
+    private ListOperations listOperations;//crud list
 
     private RedisTemplate redisTemplate;
 
     public EmployeeRepository(RedisTemplate redisTemplate) {
 
         this.hashOperations = redisTemplate.opsForHash();
+        this.listOperations = redisTemplate.opsForList();
         this.redisTemplate = redisTemplate;
 
     }
 
     public void saveEmployee(Employee employee) {
+//        crud hash
+//        hashOperations.put(HASH_KEY, employee.getId(), employee);
 
-        hashOperations.put("EMPLOYEE", employee.getId(), employee);
+//        crud list
+        listOperations.leftPush(LIST_KEY, employee);
     }
 
     public List<Employee> findAll() {
+//        crud hash
+//        return hashOperations.values(HASH_KEY);
 
-        return hashOperations.values("EMPLOYEE");
+//        crud list
+        Long lastIndex = listOperations.size(LIST_KEY) - 1;
+        return listOperations.range(LIST_KEY, 0, lastIndex);
     }
 
     public Employee findById(Integer id) {
+//        crud hash
+//        return (Employee) hashOperations.get(HASH_KEY, id);
 
-        return (Employee) hashOperations.get("EMPLOYEE", id);
+//        crud list
+        List<Employee> employees = findAll();
+        for (Employee employee : employees) {
+            if (employee.getId() == id)
+                return employee;
+        }
+
+        return new Employee();
     }
 
     public void update(Employee employee) {
@@ -41,6 +63,10 @@ public class EmployeeRepository {
     }
 
     public void delete(Integer id) {
-        hashOperations.delete("EMPLOYEE", id);
+//        crud hash
+//        hashOperations.delete(HASH_KEY, id);
+
+//        crud list
+        listOperations.rightPopAndLeftPush(LIST_KEY, id);
     }
 }
